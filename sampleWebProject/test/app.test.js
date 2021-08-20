5e3939701e8049b5f9144328e2b981ac6832f70e
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-08-21 01:28:39 +0430

    stoped ignoring test files

---
+++ sampleWebProject/test/app.test.js
@@ -0,0 +1,39 @@
+const assert = require("assert");
+
+it("has a text input", async () => {
+  const dom = await render("index.html");
+
+  const input = dom.window.document.querySelector("input");
+
+  assert(input);
+});
+
+it("shows a success message with a valid email", async () => {
+  const dom = await render("index.html");
+
+  const input = dom.window.document.querySelector("input");
+  input.value = "blahblah@blah.com";
+
+  dom.window.document
+    .querySelector("form")
+    .dispatchEvent(new dom.window.Event("submit"));
+
+  const h1 = dom.window.document.querySelector("h1");
+
+  assert.strictEqual(h1.innerHTML, "Looks Good!");
+});
+
+it("shows a fail message with a invalid email", async () => {
+  const dom = await render("index.html");
+
+  const input = dom.window.document.querySelector("input");
+  input.value = "blahblahb.com";
+
+  dom.window.document
+    .querySelector("form")
+    .dispatchEvent(new dom.window.Event("submit"));
+
+  const h1 = dom.window.document.querySelector("h1");
+
+  assert.strictEqual(h1.innerHTML, "Invalid Email!");
+});
